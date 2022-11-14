bfaeb36cc2bd3cfff8e62dae67356d0fe48d232d
capJavert <ante@kickass.website>
2022-11-14 13:42:48 +0000

    feat: move to promise based APIs

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,13 +2,11 @@ const main = async () => {
     // add/modify your content script logic here
 
     // example of how to send a message to background worker
-    chrome.runtime.sendMessage(chrome.runtime.id, { type: 'ping' }, response => {
-        // handle other message types here
+    const response = await chrome.runtime.sendMessage(chrome.runtime.id, { type: 'ping' })
 
-        if (response.type === 'pong') {
-            console.log(`Browser extensions says: "${response.payload.message}"`)
-        }
-    })
+    if (response.type === 'pong') {
+        console.log(`Browser extensions says: "${response.payload.message}"`)
+    }
 }
 
 main()
